380d3dfd44b1c61ff9c2fa0804a40f69f155691a
544-byte <544@cifpceuta.es>
2026-01-14 19:13:14 +0100

    test

--- src/Actividad1/Main.java
+++ src/Actividad1/Main.java
@@ -3,5 +3,6 @@ package Actividad1;
 public class Main {
     public static void main(String[] args) {
         System.out.println(UsoWrapper.toInt("1rtgh dgf  df23  dgf dgf dgf4aasd5"));
+
     }
 }
